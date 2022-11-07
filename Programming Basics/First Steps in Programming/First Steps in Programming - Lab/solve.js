76952d081a7f14bc8a821648ce87254cb65f3816
Stiliyan Emilov <112586793+whoisstilka@users.noreply.github.com>
2022-11-07 20:42:37 +0200

    Delete solve.js

--- Programming Basics/First Steps in Programming/First Steps in Programming - Lab/solve.js
+++
@@ -1,9 +0,0 @@
-unction solve(){
-    let firstName = "Maria";
-    let lastName = "Ivanova";
-    let age = 19;
-    let str = firstName + " " + lastName + " @ " + age;
-    console.log(str);
-}
-
-solve();
