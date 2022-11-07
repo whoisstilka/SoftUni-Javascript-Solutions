cd549687f923e2914e9ecc29401b21fcb4977e78
Stiliyan Emilov <112586793+whoisstilka@users.noreply.github.com>
2022-11-07 20:31:30 +0200

    Create animalType.js

---
+++ Programming Basics/More Complex Tests/More Complex Tests - Lab/animalType.js
@@ -0,0 +1,13 @@
+function animalType(input) {
+    let type = input[0];
+
+    switch (type) {
+        case "dog": console.log("mammal"); break;
+        case "crocodile":
+        case "tortoise":
+        case "snake": console.log("reptile"); break;
+        default: console.log("unknown");
+    }
+}
+
+animalType(["cat"]);
