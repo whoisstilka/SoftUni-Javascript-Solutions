8c7004c09683d3dfde85682c7944cd1f6ee122fa
Stiliyan Emilov <112586793+whoisstilka@users.noreply.github.com>
2022-11-07 20:35:26 +0200

    Add files via upload

---
+++ Programming Basics/Tests/Tests - Lab/excellentResult.js
@@ -0,0 +1,9 @@
+function excellentResult(input) {
+    let grade = Number(input[0]);
+    
+    if(grade >= 5.50) {
+        console.log("Excellent!");
+    }
+}
+
+excellentResult(["6.00"]);
---
+++ Programming Basics/Tests/Tests - Lab/solve.js
@@ -0,0 +1,12 @@
+function proverki(){
+    let currentDay = "Monday";
+    let salary = 0;
+
+    if (currentDay === "Monday") {
+        salary = 1000;
+    }
+
+    console.log(salary);
+}
+
+proverki();
