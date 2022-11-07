daec5e1af166d0e55a98cd0c0bbe90d456856329
Stiliyan Emilov <112586793+whoisstilka@users.noreply.github.com>
2022-11-07 20:41:33 +0200

    Create concatenateDate.js

---
+++ Programming Basics/First Steps in Programming/First Steps in Programming - Lab/concatenateDate.js
@@ -0,0 +1,10 @@
+function concatenateDate(input){
+    let firstName = input[0];
+    let lastName = input[1];
+    let age = Number(input[2]);
+    let town = input[3];
+    let result = `You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`;
+    console.log(result);
+}
+
+concatenateDate(['Petar', 'Petrov', '24', 'Sofia'])
