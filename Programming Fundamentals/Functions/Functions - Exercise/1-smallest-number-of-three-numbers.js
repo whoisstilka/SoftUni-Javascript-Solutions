4394322aec476dd48ae01dd4e3da395b338c0d51
Stiliyan Emilov <112586793+whoisstilka@users.noreply.github.com>
2022-11-08 19:42:45 +0200

    Create 1-smallest-number-of-three-numbers.js

---
+++ Programming Fundamentals/Functions/Functions - Exercise/1-smallest-number-of-three-numbers.js
@@ -0,0 +1,9 @@
+function smallestNumber(numOne, numTwo, numThree) {
+    console.log(Math.min(numOne, numTwo, numThree));
+}
+
+smallestNumber(2, 5, 3);
+smallestNumber(600, 342, 123);
+smallestNumber(25, 21, 4);
+smallestNumber(2, 2, 2);
+
