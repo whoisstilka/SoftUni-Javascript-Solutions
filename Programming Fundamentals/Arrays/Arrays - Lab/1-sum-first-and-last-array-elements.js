fec7dce94b81831f2e94c8d787e41fb8410aa79a
Stiliyan Emilov <112586793+whoisstilka@users.noreply.github.com>
2022-11-08 19:35:14 +0200

    Create 1-sum-first-and-last-array-elements.js

---
+++ Programming Fundamentals/Arrays/Arrays - Lab/1-sum-first-and-last-array-elements.js
@@ -0,0 +1,10 @@
+function sumElements(numbers) {
+    let firstNum = numbers[0];
+    let lastNum = numbers[numbers.length - 1];
+
+    console.log(firstNum + lastNum);
+}
+
+sumElements([20, 30, 40]);
+sumElements([10, 17, 22, 33]);
+sumElements([11, 58, 69]);
